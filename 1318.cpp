dc655ec3c87000ad3c921e0d6f411741a0e5a270
gwy15 <gwy15thu@gmail.com>
2020-01-15 15:06:54 +0800

    1318

---
+++ 1318.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Solution {
+   public:
+    int constexpr inline sumLastByte(const int x) {
+        const constexpr int BITS[] = {
+            0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4, 1, 2, 2, 3, 2, 3,
+            3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4,
+            3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 1, 2,
+            2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5,
+            3, 4, 4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5,
+            5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 1, 2, 2, 3,
+            2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4,
+            4, 5, 4, 5, 5, 6, 2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+            3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 2, 3, 3, 4, 3, 4,
+            4, 5, 3, 4, 4, 5, 4, 5, 5, 6, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6,
+            5, 6, 6, 7, 3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7, 4, 5,
+            5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8};
+        return BITS[x];
+    }
+
+    int constexpr inline sumBits(const int x) {
+        return sumLastByte(x & 0xFF) + sumLastByte((x >> 8) & 0xFF) +
+               sumLastByte((x >> 16) & 0xFF) + sumLastByte((x >> 24) & 0xFF);
+    }
+
+    int minFlips(int a, int b, int c) {
+        int sum = sumBits((a | b) ^ c) + sumBits((a & b) & (~c));
+        return sum;
+    }
+};
+
+int main() {
+    std::cout << Solution().minFlips(631, 507, 219) << std::endl;
+    return 0;
+}
