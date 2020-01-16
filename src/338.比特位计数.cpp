f1e31130af71ccff1c5a244b07c82f07b815c176
gwy15 <gwy15thu@gmail.com>
2020-01-16 15:31:10 +0800

    191 338

---
+++ src/338.比特位计数.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=338 lang=cpp
+ *
+ * [338] 比特位计数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num+1, 0);
+        for (int i=1; i<=num; i++) {
+            res[i] = res[i >> 1] + (i & 1);
+        }
+        return res;
+    }
+};
+// @lc code=end
+
