a1c60100f25dfe634747b1c2d7f7ec94adad3c1d
gwy15 <gwy15thu@gmail.com>
2020-04-29 11:51:09 +0800

    406

--- src/406.根据身高重建队列.cpp
+++ src/406.根据身高重建队列.cpp
@@ -6,11 +6,11 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-using std::cout, std::endl;
+using std::cout;
+using std::endl;
 using std::vector;
 
 // @lc code=start
-#include <cassert>
 class Solution {
     int lowbit(int x) { return x & (-x); }
 
@@ -20,6 +20,24 @@ class Solution {
             ret <<= 1;
         }
         return ret;
+    }
+
+    int binary_search_bit_for_sum(const vector<int>& BIT, int target_sum,
+                                  int init_incr) {
+        int n = BIT.size() - 1;
+        int cur_sum = 0, cur_pos = 0, incr = init_incr;
+        while (incr) {
+            int try_pos = cur_pos + incr;
+            if (try_pos <= n) {
+                if (cur_sum + BIT[try_pos] < target_sum) {
+                    cur_sum += BIT[try_pos];
+                    cur_pos = try_pos;
+                }
+            }
+            incr /= 2;
+        }
+        // +1 because cur_sum < target_sum. +1 makes equal
+        return cur_pos + 1;
     }
 
    public:
@@ -33,34 +51,20 @@ class Solution {
                       }
                       return p1[0] > p2[0];
                   });
-        // 树状数组
+        // init BIT
         vector<int> BIT = vector<int>(n + 1);
         for (int i = 1; i <= n; i++) {
             BIT[i] = lowbit(i);
         }
 
-        // reversely insert every h into k th pos
+        // reversely insert every people into k-th pos
         vector<vector<int>> result(n);
-        int highbit_n = highbit(n);
+        // cache a log n operation
+        const int highbit_n = highbit(n);
         for (int i = n - 1; i >= 0; i--) {
-            int h = people[i][0];
-            int target_sum = people[i][1] + 1;
+            const int target_sum = people[i][1] + 1;
             // find pos to insert
-            int cur_sum = 0, cur_pos = 0, incr = highbit_n;
-            while (incr) {
-                int try_pos = cur_pos + incr;
-                if (try_pos <= n) {
-                    if (cur_sum + BIT[try_pos] < target_sum) {
-                        cur_sum += BIT[try_pos];
-                        cur_pos = try_pos;
-                    } else {
-                        // cur_sum -= BIT[try_pos];
-                    }
-                }
-                incr /= 2;
-            }
-            // offset is 1!
-            int pos = cur_pos + 1;
+            int pos = binary_search_bit_for_sum(BIT, target_sum, highbit_n);
             // clear pos (mark as inserted)
             for (int j = pos; j <= n; j += lowbit(j)) {
                 BIT[j] -= 1;
