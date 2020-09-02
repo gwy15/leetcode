6f18c99e2c333c9322b709471e8a4f9c7ea8bdde
gwy15 <gwy15thu@gmail.com>
2020-09-02 22:09:16 +0800

    217.

---
+++ src/217.存在重复元素.py
@@ -0,0 +1,11 @@
+#
+# @lc app=leetcode.cn id=217 lang=python3
+#
+# [217] 存在重复元素
+#
+
+# @lc code=start
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+# @lc code=end
