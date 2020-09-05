e9d0606c95ce81831a6b43e7fc39a6042f6acc62
gwy15 <gwy15thu@gmail.com>
2020-09-05 12:37:14 +0800

    292.

---
+++ src/292.nim-游戏.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode.cn id=292 lang=python3
+#
+# [292] Nim 游戏
+#
+
+# @lc code=start
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return bool(n % 4)
+
+# @lc code=end
