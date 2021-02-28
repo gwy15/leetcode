82dc786ccb6415b9271013ff65ef71be24abf3ad
管唯宇 <gwy15thu@gmail.com>
2021-02-28 17:24:24 +0800

    32.

--- src/32.最长有效括号.py
+++ src/32.最长有效括号.py
@@ -3,73 +3,36 @@
 #
 # [32] 最长有效括号
 #
-
+from prelude import *
 # @lc code=start
 
 
 class Solution:
-    def longestValidParentheses_greedy(self, s: str) -> int:
-        ans = 0
-
-        left, right = 0, 0
-        for ch in s:
-            if ch == '(':
-                left += 1
-            else:
-                right += 1
-
-            if left == right:
-                ans = max(ans, 2 * right)
-            elif left < right:
-                left, right = 0, 0
-
-        left, right = 0, 0
-        for ch in s[::-1]:
-            if ch == '(':
-                left += 1
-            else:
-                right += 1
-
-            if left == right:
-                ans = max(ans, 2 * right)
-            elif left > right:
-                left, right = 0, 0
-
-        return ans
-
-    def longestValidParentheses_stack(self, s: str) -> int:
-        ans = 0
-
-        # stack looks like ')((((' (for index)
-        stack = []
-        stack.append(-1)
-        for idx, ch in enumerate(s):
-            if ch == '(':
-                stack.append(idx)
-            else:  # ')'
-                # stack is not empty
-                stack.pop()
-                if len(stack) == 0:  # popped last )
-                    # make this the new )
-                    stack.append(idx)
-                else:
-                    top = stack[-1]
-                    ans = max(ans, idx - top)
-
-        return ans
-
     def longestValidParentheses(self, s: str) -> int:
-        # return self.longestValidParentheses_greedy(s)
-        return self.longestValidParentheses_stack(s)
+        stack = [-1]
+        best = 0
+        for i, ch in enumerate(s):
+            if ch == '(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if len(stack) > 0:
+                    best = max(best, i - stack[-1])
+                else:
+                    # illegal start at i
+                    stack.append(i)
+        return best
 
 
 # @lc code=end
-if __name__ == "__main__":
-    f = Solution().longestValidParentheses
-    assert f('(()') == 2
-    assert f(')()())') == 4
-    assert f('') == 0
-    assert f(')(') == 0
-    assert f('(((((((((((((((((()') == 2
-    assert f('))))))))((())))') == 6
-    assert f('()(()') == 2
+if __name__ == '__main__':
+    def test(input, expected):
+        calc = Solution().longestValidParentheses(input)
+        if calc != expected:
+            print(f'case failed: `{input}`')
+            print(f'  calc     = `{calc}`')
+            print(f'  expected = `{expected}`')
+            exit(1)
+    test('(()', 2)
+    test(')()())', 4)
+    test('', 0)
