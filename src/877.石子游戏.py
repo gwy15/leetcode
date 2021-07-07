f35deb688aa2d02114dbcb6ff527695ba75fa24f
gwy15 <gwy15thu@gmail.com>
2021-07-07 19:31:46 +0800

    877

---
+++ src/877.石子游戏.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode.cn id=877 lang=python3
+#
+# [877] 石子游戏
+#
+from prelude import *
+# @lc code=start
+
+
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        return True
+# @lc code=end
