79d9610021d40a7e2f793dda4e4773676bec29f6
gwy15 <gwy15thu@gmail.com>
2021-07-22 17:58:05 +0800

    11。

--- src/11.盛最多水的容器.rs
+++ src/11.盛最多水的容器.rs
@@ -9,30 +9,38 @@ impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
         let n = height.len();
         let (mut l, mut r) = (0, n - 1);
-        let mut max = 0;
+        let mut ans = 0;
         while l < r {
-            max = max.max((r - l) as i32 * i32::min(height[l], height[r]));
-            if height[l] < height[r] {
-                // move l
-                l += 1;
+            let h_l = height[l];
+            let h_r = height[r];
+            ans = ans.max(h_l.min(h_r) * (r - l) as i32);
+            // 移动小的那个
+            if h_l < h_r {
+                while l < r && height[l] <= h_l {
+                    l += 1;
+                }
             } else {
-                r -= 1;
+                while l < r && height[r] <= h_r {
+                    r -= 1;
+                }
             }
         }
-        max
+        ans
     }
 }
 // @lc code=end
 #[test]
 fn test_solution() {
     macro_rules! test {
-        ($t:tt, $a:expr) => {
+        ($args:tt, $ans:expr) => {
             assert_eq!(
-                Solution::max_area(vec!$t),
-                $a
-            );
-        }
-    };
+                Solution::max_area(vec!$args),
+                $ans
+            )
+        };
+    }
     test!([1, 8, 6, 2, 5, 4, 8, 3, 7], 49);
-    test!([1, 2], 1);
+    test!([1, 1], 1);
+    test!([4, 3, 2, 1, 4], 16);
+    test!([1, 2, 1], 2);
 }
